e1d3c4f9d75444a154ab141060c56a48559a1929
thanh.bien0402 <thanh.bien0402@hcmut.edu.vn>
2025-05-30 02:30:23 +0700

    add arduino template

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -2,96 +2,6 @@
 #include <DHT20.h>
 #include <LiquidCrystal_I2C.h>;
 
-#include <WiFi.h>
-#include <WebServer.h>
-#include <Preferences.h>
-
-Preferences preferences;
-WebServer server(80);
-String selectedSSID = "";
-String selectedPassword = "";
-
-void saveWiFiToNVS(const String& ssid, const String& password) {
-  preferences.begin("wifi", false);
-  preferences.putString("ssid", ssid);
-  preferences.putString("password", password);
-  preferences.end();
-}
-
-void loadWiFiFromNVS(String& ssid, String& password) {
-  preferences.begin("wifi", true);
-  ssid = preferences.getString("ssid", "");
-  password = preferences.getString("password", "");
-  preferences.end();
-}
-
-bool connectToWiFi(const String& ssid, const String& password, int maxRetries) {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid.c_str(), password.c_str());
-
-  Serial.printf("🔌 Đang kết nối tới %s", ssid.c_str());
-  for (int i = 0; i < maxRetries; ++i) {
-    if (WiFi.status() == WL_CONNECTED) {
-      Serial.println("\n✅ Đã kết nối Wi-Fi!");
-      Serial.println("📶 IP: " + WiFi.localIP().toString());
-      return true;
-    }
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println("\n❌ Không thể kết nối Wi-Fi.");
-  return false;
-}
-
-void stopAPMode() {
-  WiFi.softAPdisconnect(true);
-  delay(500);
-}
-
-// #include "Adafruit_MQTT.h"
-// #include "Adafruit_MQTT_Client.h"
-
-
-
-// #define WLAN_SSID "abcd"
-// #define WLAN_PASS "12345678"
-// #define AIO_SERVER      "io.adafruit.com"
-// #define AIO_SERVERPORT  1883
-// #define AIO_USERNAME    "NPNLab_BBC"
-// #define AIO_KEY         ""
-
-// #define OHS_SERVER      "mqtt.ohstem.vn"
-// #define OHS_SERVERPORT  1883
-// #define OHS_USERNAME    "abcd0386433465"
-// #define OHS_KEY         "12345678"
-
-
-// #define BKTK_SERVER      "mqttserver.tk"
-// #define BKTK_SERVERPORT  1883
-// #define BKTK_USERNAME    "innovation"
-// #define BKTK_KEY         "Innovation_RgPQAZoA5N"
-
-// WiFiClient client;
-// //Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_USERNAME, AIO_KEY);
-// //Adafruit_MQTT_Client mqtt(&client, OHS_SERVER, OHS_SERVERPORT, OHS_USERNAME, OHS_USERNAME, OHS_KEY);
-// Adafruit_MQTT_Client mqtt(&client, BKTK_SERVER, BKTK_SERVERPORT, BKTK_USERNAME, BKTK_USERNAME, BKTK_KEY);
-
-/****************************** Feeds ***************************************/
-
-// // Setup a feed called 'time' for subscribing to current time
-// Adafruit_MQTT_Subscribe timefeed = Adafruit_MQTT_Subscribe(&mqtt, "time/seconds");
-
-// // Setup a feed called 'slider' for subscribing to changes on the slider
-// Adafruit_MQTT_Subscribe slider = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/slider", MQTT_QOS_1);
-
-// // Setup a feed called 'onoff' for subscribing to changes to the button
-// //Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoff", MQTT_QOS_1);
-// Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, OHS_USERNAME "/feeds/V1", MQTT_QOS_1);
-
-// // Setup a feed called 'photocell' for publishing.
-// //Adafruit_MQTT_Publish sensory = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/sensory");
-// Adafruit_MQTT_Publish sensory = Adafruit_MQTT_Publish(&mqtt, OHS_USERNAME "/feeds/V20");
-
 // Define your tasks here
 void TaskBlink(void *pvParameters);
 void TaskTemperatureHumidity(void *pvParameters);
@@ -103,41 +13,6 @@ Adafruit_NeoPixel pixels3(4, D5, NEO_GRB + NEO_KHZ800);
 DHT20 dht20;
 LiquidCrystal_I2C lcd(33,16,2);
 
-// void slidercallback(double x) {
-//   Serial.print("Hey we're in a slider callback, the slider value is: ");
-//   Serial.println(x);
-// }
-
-// void onoffcallback(char *data, uint16_t len) {
-//   Serial.print("Hey we're in a onoff callback, the button value is: ");
-//   Serial.println(data);
-// }
-
-// void MQTT_connect() {
-//   int8_t ret;
-
-//   // Stop if already connected.
-//   if (mqtt.connected()) {
-//     return;
-//   }
-
-//   Serial.print("Connecting to MQTT... ");
-
-//   uint8_t retries = 3;
-//   while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-//     Serial.println(mqtt.connectErrorString(ret));
-//     Serial.println("Retrying MQTT connection in 10 seconds...");
-//     mqtt.disconnect();
-//     delay(10000);  // wait 10 seconds
-//     retries--;
-//     if (retries == 0) {
-//       // basically die and wait for WDT to reset me
-//       while (1);
-//     }
-//   }
-//   Serial.println("MQTT Connected!");
-// }
-
 void setup() {
 
   // Initialize serial communication at 115200 bits per second:
@@ -146,22 +21,6 @@ void setup() {
   dht20.begin();
   lcd.begin();
   pixels3.begin();
-
-  // WiFi.begin(WLAN_SSID, WLAN_PASS);
-  // delay(2000);
-
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(500);
-  //   Serial.print(".");
-  // }
-  // Serial.println();
-  // Serial.print("IP address: ");
-  // Serial.println(WiFi.localIP());
-
-  // slider.setCallback(slidercallback);
-  // onoffbutton.setCallback(onoffcallback);
-  // mqtt.subscribe(&slider);
-  // mqtt.subscribe(&onoffbutton);
 
   xTaskCreate( TaskBlink, "Task Blink" ,2048  ,NULL  ,2 , NULL);
   xTaskCreate( TaskTemperatureHumidity, "Task Temperature" ,2048  ,NULL  ,2 , NULL);
@@ -173,13 +32,8 @@ void setup() {
   Serial.printf("Basic Multi Threading Arduino Example\n");
 }
 
-// int pubCount = 0;
 void loop() {
-  // MQTT_connect();
-  // mqtt.processPackets(10000);
-  // if(! mqtt.ping()) {
-  //   mqtt.disconnect();
-  // }
+
 }
 
 
@@ -201,9 +55,6 @@ void TaskBlink(void *pvParameters) {  // This is a task.
     delay(1000);
     digitalWrite(LED_BUILTIN, LOW);  // turn the LED OFF
     delay(1000);
-    // if (sensory.publish(x++)) {
-    //   Serial.println(F("Published successfully!!"));
-    // }
   }
 }
 
